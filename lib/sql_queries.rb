6373e7f9f26ed72dc2750a9c0eccbea1bd423df4
Alyssa Yates <bosslys@icloud.com>
2019-10-13 00:08:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,8 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAXcount(color) AS bears FROM bears GROUP BY color ORDER BY bears DESC"
+  <-- SQL
+  SQL
 end
 
 def counts_number_of_bears_with_goofy_temperaments
