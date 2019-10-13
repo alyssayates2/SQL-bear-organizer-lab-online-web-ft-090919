47325ce117218c09a65279fbc81306f99ae76728
Alyssa Yates <bosslys@icloud.com>
2019-10-13 00:13:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,12 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   <-- SQL
-  SQL
+      SELECT color, COUNT(color)
+      FROM bears 
+      GROUP BY color 
+      ORDER BY COUNT(color) DESC 
+      LIMIT 1
+    SQL
 end
 
 def counts_number_of_bears_with_goofy_temperaments
